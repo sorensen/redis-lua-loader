f652431386387baba4bd3c117bd31dcfe25a5c6a
Beau Sorensen <mail@beausorensen.com>
2013-04-20 15:10:56 -0700

    Initial project creation

---
+++ test/index.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var assert = require('assert')
+  , ase = assert.strictEqual
+  , fs = require('fs')
+  , Lua = require('../lib/lua')
+  , utils = require('../lib/utils')
+  , redis = require('redis')
+  , path = __dirname + '/lua'
+  , files = fs.readdirSync(path)
+
+describe('Lua', function() {
+  var db = redis.createClient()
+    , lua = new Lua(db, {dirname: path})
+
+  it('should emit a `ready` event', function(done) {
+    lua.ready ? done() : lua.on('ready', done)
+  })
+
+  it('should connect to redis', function(done) {
+    db.ready ? done() : db.on('ready', done)
+  })
+
+  it('should have loaded all lua scripts', function() {
+    ase(files.length, Object.keys(lua.shas).length)
+    files.forEach(function(file) {
+      ase(typeof lua.shas[file.replace('.lua', '')], 'string')
+    })
+  })
+
+  it('should create a script wrapper function', function() {
+    var fn = lua.wrapScript('test')
+    ase(typeof fn, 'function')
+  })
+
+  it('should have created a method for each script loaded', function() {
+    files.forEach(function(file) {
+      var name = utils.camelCase(file.replace('.lua', ''))
+      ase(typeof lua[name], 'function')
+    })
+  })
+
+  it('should work with a custom directory', function(done) {
+    var lua2 = new Lua(db, {
+      dirname: __dirname + '/lua2'
+    })
+    lua2.on('ready', function() {
+      ase(typeof lua2.shas['return-one'], 'string')
+      ase(typeof lua2.returnOne, 'function')
+      done()
+    })
+  })
+
+  it('should load multiple directories', function(done) {
+    var lua3 = new Lua(db, {
+      dirname: [
+        __dirname + '/lua'
+      , __dirname + '/lua2'
+      ]
+    })
+    lua3.on('ready', function() {
+      ase(typeof lua3.shas.test, 'string')
+      ase(typeof lua3.test, 'function')
+      files.forEach(function(file) {
+        file = file.replace('.lua', '')
+        ase(typeof lua3.shas[file], 'string')
+        ase(typeof lua3[utils.camelCase(file)], 'function')
+      })
+      done()
+    })
+  })
+
+  it('should throw when wrapping a non-loaded script', function() {
+    assert.throws(function() {
+      lua.wrapScript('foobar')
+    })
+  })
+
+  it('should disconnect from redis', function(done) {
+    db.on('end', done)
+    db.quit()
+  })
+})
